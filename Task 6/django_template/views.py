81032946c3634b9e1fdf42e77df2afcf4eee7fb8
Philip Okiokio <philipokiokio@gmail.com>
2021-04-20 06:46:51 +0100

    variables

--- Task 6/django_template/views.py
+++ Task 6/django_template/views.py
@@ -3,7 +3,13 @@ from .models import bookStore
 
 # Create your views here.
 def home_view(request):
-    book= get_object_or_404(bookStore,year_published= 2020)
-    book_author = get_object_or_404(bookStore,book_author='Chinua Achebe')
-    return render(request, 'home.html', {'book':book,
-                                        'book_author':book_author})
+    book= bookStore.objects.get(year_published= 2020)
+    book_author = bookStore.objects.get(book_author='Chinua Achebe')
+    all_books = bookStore.objects.all()
+    return render(request, 'home.html',
+     {
+     'book':book,
+     'book_author':book_author,
+     'all_books':all_books
+                                        }
+                                        )
